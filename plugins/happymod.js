8803ee5fcb4cde55d7009f383e6f57cc4157c32a
ANIRUDH <78668573+anirudhsudheer@users.noreply.github.com>
2021-07-27 09:28:13 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -14,14 +14,16 @@ const Lang = Language.getString('weather');
 
 Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
+	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=SUcKEWdDpYjtj2q045KNQCoDyCe`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
-		'*📕 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' + 
-		'*📘 ' + Lang.SIZE +'* ```' + json.result[0].size + '```\n\n\n' + 
-		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].link + '```\n', MessageType.text);
+		'*📕 ' + Lang.NAME +'* ```' + json.result[0].title + '```\n\n' + 
+		'*📘 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' + 
+		'*📘 ' + Lang.IMAGE +'* ```' + json.result[0].thumb + '```\n\n\n' + 									 
+		'*📘 ' + Lang.SIZE +'* ```' + json.result[0].size + '```\n\n\n' + 									 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n', MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
