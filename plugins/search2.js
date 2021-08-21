67895672d7202066fe28b18b26ffed6cb8576b29
ANIRUDH <anirudhsudheer@gmail.com>
2021-08-21 23:05:04 +0530

    Update search2.js

--- plugins/search2.js
+++ plugins/search2.js
@@ -11,16 +11,15 @@ const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
-
 Asena.addCommand({pattern: 'srch(.*)', fromMe: false, desc: "Search In Google" }, async (message, match) => {
-	if (match[1] === '') return await message.reply(Give Me Some Words To Search...);
+	if (match[1] === '') return await message.reply("Give Me Some Words To Search...");
 	const url = `https://api.lolhuman.xyz/api/gsearch?apikey=7187e9cee7fada1e3c47025c&query=${match[1]}`;
 	const response = await got(url);
 	const json = JSON.parse(response.body);
 	if (json.Response != 'True') return await message.client.sendMessage(message.jid, '*Not found.*', MessageType.text, { quoted: message.data });
 	let msg = '```';
 	msg += 'Title      : ' + json.title + '\n\n';
-    msg += 'Description  : ' + json.desc + '\n\n';
+        msg += 'Description  : ' + json.desc + '\n\n';
 	msg += 'Link  : ' + json.link + '```';
 	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
 }));
